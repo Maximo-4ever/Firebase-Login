2b5661403f406f17583bfaa2abf4101e3b17a24e
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-28 14:49:17 -0600

    Permite al usuario cerrar sesion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
+import { useAuth } from "../context/authContext";
+
 export default function Home() {
+  const { user, logout, loading } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome {user.email}</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
