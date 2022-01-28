33caaabdaaef35c78c30783faaac8586731accfe
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-28 14:48:16 -0600

    Agrega a Home a las rutas protegidas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Register from "./components/Register";
 import { AuthProvider } from "./context/authContext";
 
@@ -10,7 +11,11 @@ function App() {
     <div className="bg-slate-300 h-screen text-black d-flex">
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
