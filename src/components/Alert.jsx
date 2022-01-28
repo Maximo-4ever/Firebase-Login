0b90d263e8a275f34bcd5ffe61f8fd61a382443c
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-28 15:33:02 -0600

    Estiliza la alerta de error

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,9 @@
 export const Alert = ({ message }) => {
   return (
-    <div className="bg-red-200">
-      <span>{message}</span>
+    <div 
+      className="bg-red-200 border border-red-400 text-red-700 px-3 py-2 text-center mb-2 relative"
+    >
+      <span className="sm:inline block capitalize">{message}</span>
     </div>
   );
 };
